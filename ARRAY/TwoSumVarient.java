f962122eea95c072fa77486ef623abd41481f67f
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-06-15 07:04:51 +0530

    added hashmap approach

---
+++ ARRAY/TwoSumVarient.java
@@ -0,0 +1,33 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+
+import java.util.HashMap;
+
+public class TwoSumVarient {
+    
+     public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+         int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
+            }
+
+            mpp.put(nums[i], i);
+        }
+        return ans;
+    }
+        
+    }
+
