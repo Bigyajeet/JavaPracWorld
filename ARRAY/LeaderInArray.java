92d1a4531ab4ccf6f87cbbbf262fe7a506ef7fd9
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-06-21 12:10:44 +0530

    optimal sol

--- ARRAY/LeaderInArray.java
+++ ARRAY/LeaderInArray.java
@@ -50,3 +50,24 @@ public static void main(String args[])
 }
 }
 
+// optimal
+
+// static ArrayList<Integer> leaders(int arr[]) {
+//         ArrayList<Integer> result = new ArrayList<>();
+//         int n = arr.length;
+
+//         int maxFromRight = arr[n - 1];
+//         result.add(maxFromRight);
+
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (arr[i] >= maxFromRight) {
+//                 maxFromRight = arr[i];
+//                 result.add(maxFromRight);
+//             }
+//         }
+
+//         Collections.reverse(result);
+
+//         return result;
+//     }
+// }
