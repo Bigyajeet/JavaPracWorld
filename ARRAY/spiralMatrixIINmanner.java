fe690a2f0e96432b3d38548989ff78baa245619a
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-06-28 11:16:52 +0530

    typecast idea learned new for arraylist

--- ARRAY/spiralMatrixIINmanner.java
+++ ARRAY/spiralMatrixIINmanner.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+//Spirally traversing a matrix
+
 
 public class spiralMatrixIINmanner {
     public static List<Integer> printSpiral(int[][] mat) {
@@ -64,5 +66,50 @@ public class spiralMatrixIINmanner {
         System.out.println();
     }
 }
+
+
+
+//arraylist
+  // Function to return a list of integers denoting spiral traversal of matrix.
+    // public ArrayList<Integer> spirallyTraverse(int mat[][]) {
+    //     // code here
+    //      List<Integer> ans = new ArrayList<>();
+        
+    //     int n = mat.length; 
+    //     int m = mat[0].length; 
+        
+    //     int top = 0, left = 0, bottom = n - 1, right = m - 1;
+    //     while (top <= bottom && left <= right) {
+    //         for (int i = left; i <= right; i++)
+    //             ans.add(mat[top][i]);
+
+    //         top++;
+    //         for (int i = top; i <= bottom; i++)
+    //             ans.add(mat[i][right]);
+
+    //         right--;
+
+            
+    //         if (top <= bottom) {
+    //             for (int i = right; i >= left; i--)
+    //                 ans.add(mat[bottom][i]);
+
+    //             bottom--;
+    //         }
+
+        
+    //         if (left <= right) {
+    //             for (int i = bottom; i >= top; i--)
+    //                 ans.add(mat[i][left]);
+
+    //             left++;
+    //         }
+    //     }
+    //     return (ArrayList<Integer>) ans;
+    // }
+    
+        
+    
+
     
 
