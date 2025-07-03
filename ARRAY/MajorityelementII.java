933879c4b7756ddcea93a1b6bc418c31aa187363
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-07-03 22:13:11 +0530

    better approach

--- ARRAY/MajorityelementII.java
+++ ARRAY/MajorityelementII.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MajorityelementII {
+    //brute
      public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
         List<Integer> ls=new ArrayList<>();
@@ -22,3 +23,22 @@ public class MajorityelementII {
     }
     
 }
+
+// better approach using hashmap
+//  public List<Integer> majorityElement(int[] nums) {
+//         int n=nums.length;
+//         List<Integer> ls=new ArrayList<>();
+//         HashMap<Integer,Integer>mpp=new HashMap<>();
+//         int mini=(int)(n/3)+1;
+//         for(int i=0;i<n;i++){
+//             int val=mpp.getOrDefault(nums[i],0);
+//             mpp.put(nums[i],val+1);
+//             if(mpp.get(nums[i])==mini){
+//                 ls.add(nums[i]);
+
+//             }
+//             if(ls.size()==2) break;
+//         }
+//         return ls;
+//     }
+// }
