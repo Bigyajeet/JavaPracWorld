33721afcf89b7dcdc8f73fda006b6a96bbeb0d8f
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-07-03 21:33:41 +0530

    brute appoach applied

---
+++ ARRAY/MajorityelementII.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityelementII {
+     public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
+        List<Integer> ls=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(ls.size()==0 || ls.get(0)!=nums[i]){
+               int cnt=0;
+                for(int j=0;j<n;j++){
+                   if(nums[j]==nums[i]){
+                    cnt++;
+                   }
+                }
+                if(cnt>(n/3))  ls.add(nums[i]);
+            }
+            if(ls.size()==2)  break;
+        }
+        return ls;
+        
+    }
+    
+}
