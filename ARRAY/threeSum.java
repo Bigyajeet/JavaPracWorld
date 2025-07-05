a676797a26e2b09e9497dc21b39f80c81adcc123
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-07-05 07:12:32 +0530

    beter approach throuch hashing

--- ARRAY/threeSum.java
+++ ARRAY/threeSum.java
@@ -5,21 +5,24 @@ import java.util.List;
 import java.util.Set;
 
 class threeSum{
+//better
 
-    //brute force approach
     public static List<List<Integer>> triplet(int n, int[] arr) {
         Set<List<Integer>> st = new HashSet<>();
 
-        // check all possible triplets:
         for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
             for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-                        temp.sort(null);
-                        st.add(temp);
-                    }
+                //Calculate the 3rd element:
+                int third = -(arr[i] + arr[j]);
+
+                //Find the element in the set:
+                if (hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
                 }
+                hashset.add(arr[j]);
             }
         }
 
@@ -42,4 +45,43 @@ class threeSum{
         System.out.println();
     }
 }
+
+
+
+//     //brute force approach
+//     public static List<List<Integer>> triplet(int n, int[] arr) {
+//         Set<List<Integer>> st = new HashSet<>();
+
+//         // check all possible triplets:
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 for (int k = j + 1; k < n; k++) {
+//                     if (arr[i] + arr[j] + arr[k] == 0) {
+//                         List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+//                         temp.sort(null);
+//                         st.add(temp);
+//                     }
+//                 }
+//             }
+//         }
+
+//         // store the set elements in the answer:
+//         List<List<Integer>> ans = new ArrayList<>(st);
+//         return ans;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { -1, 0, 1, 2, -1, -4};
+//         int n = arr.length;
+//         List<List<Integer>> ans = triplet(n, arr);
+//         for (List<Integer> it : ans) {
+//             System.out.print("[");
+//             for (Integer i : it) {
+//                 System.out.print(i + " ");
+//             }
+//             System.out.print("] ");
+//         }
+//         System.out.println();
+//     }
+// }
 
